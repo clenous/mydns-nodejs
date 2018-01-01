5c5b45b1a57549b12333b3a3a45782ec252b60d1
Hirano Takanori <hrn.takanori@gmail.com>
2018-01-01 15:57:29 +0900

    環境変数から拾うように修正

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
+const {ID, PASSWORD, SLACK_URL} = process.env;
+
 module.exports = {
   cron: '00 * * * *',
   timeZone: 'Asia/Tokyo',
   accounts:[
     {
-      MasterID: 'mydns******',
-      Password: '***********',
+      ID: ID || 'mydns******',
+      Password: PASSWORD || '***********',
       protocol: 'ipv4',
       //all ipv6 ipv4
       notify: [
         {
           type: 'slack',
-          url: 'https://hooks.slack.com/services/**************************************',
+          url: SLACK_URL || 'https://hooks.slack.com/services/**************************************',
           locale: 'en',
           format: '{ID}\n{ADDR}'
         },
