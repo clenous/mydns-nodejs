ac23b9af9c9f008fe628d98bbc3cb8223349ed8f
Hirano Takanori <hrn.takanori@gmail.com>
2018-01-01 16:54:34 +0900

    add config sample

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
-const {ID, PASSWORD, SLACK_URL} = process.env;
+// ex) CONFIG=[{"ID":"mydns*******","PASSWORD":"******","SLACK_URL":"https://hooks.slack.com/services/**************************************"}]
+const CONFIG = JSON.parse(process.env.CONFIG)
 
-module.exports = {
+const config = {
   cron: '00 * * * *',
   timeZone: 'Asia/Tokyo',
   accounts:[
     {
-      ID: ID || 'mydns******',
-      Password: PASSWORD || '***********',
-      protocol: 'ipv4',
+      ID: 'mydns******',
+      Password: '***********',
+      protocol: 'all',
       //all ipv6 ipv4
       notify: [
         {
           type: 'slack',
-          url: SLACK_URL || 'https://hooks.slack.com/services/**************************************',
+          url: 'https://hooks.slack.com/services/**************************************',
           locale: 'en',
           format: '{ID}\n{ADDR}'
         },
@@ -24,4 +25,21 @@ module.exports = {
       ]
     }
   ]
-};
+}
+
+for(let i in CONFIG) {
+  config.accounts[i] = {
+    ID: CONFIG[i].ID,
+    Password: CONFIG[i].PASSWORD
+  }
+  if(CONFIG[i].SLACK_URL!=null) {
+    config.accounts[i].notify.push({
+      type: 'slack',
+      url: CONFIG[i].SLACK_URL,
+      locale: 'en',
+      format: '{ID}\n{ADDR}'
+    })
+  }
+}
+
+module.exports = config
---
+++ ecosystem.config.sample.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: 'mydns',
+      script: 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy: {
+    HOSTNAME : {
+      user: 'node',
+      host: 'HOSTNAME',
+      port: '22',
+      env: {
+        CONFIG: '[{"ID":"mydns*******","PASSWORD":"******","SLACK_URL":"https://hooks.slack.com/services/**************************************"}]'
+      },
+      ref: 'origin/master',
+      repo: 'https://github.com/hrntknr/mydns-nodejs.git',
+      path: '/var/www/mydns-nodejs',
+      'post-deploy': '\
+        npm install && \
+        pm2 reload pm2.json --env production \
+      '
+    }
+  }
+}
