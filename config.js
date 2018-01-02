60e975c5b7f3d2b72be25ff1114b0a17c524a094
Hirano Takanori <hrn.takanori@gmail.com>
2018-01-02 15:41:43 +0900

    update config

--- config.js
+++ config.js
@@ -1,21 +1,36 @@
-const {ID, PASSWORD, SLACK_URL} = process.env;
-
-module.exports = {
-  cron: '00 * * * *',
-  timeZone: 'Asia/Tokyo',
-  accounts:[
-    {
-      ID: ID || 'mydns******',
-      Password: PASSWORD || '***********',
-      protocol: 'all',
-      format: '{ID}\n{ADDR}',
-      //all ipv6 ipv4
-      notify: [
+module.exports = (()=>{
+  if(process.env.ID) {
+    const {ID, PASSWORD, SLACK_URL} = process.env
+    return {
+      cron: '00 * * * *',
+      timeZone: 'Asia/Tokyo',
+      accounts:[
         {
-          type: 'slack',
-          url: SLACK_URL || 'https://hooks.slack.com/services/**************************************'
+          ID: ID || 'mydns******',
+          Password: PASSWORD || '***********',
+          protocol: 'all',
+          format: '{ID}\n{ADDR}',
+          //all ipv6 ipv4
+          notify: SLACK_URL
+            ?[{type: 'slack', url: SLACK_URL || 'https://hooks.slack.com/services/**************************************'}]
+            :[]
         }
       ]
     }
-  ]
-}
+  }else {
+    const accounts = []
+    for(let i=1;true;i++) { //eslint-disable-line
+      if(process.env[`ID_${i}`])break
+      accounts.push({
+        ID: process.env[`ID_${i}`],
+        Password: process.env[`PASSWORD_${i}`],
+        protocol: 'all',
+        format: '{ID}\n{ADDR}',
+        //all ipv6 ipv4
+        notify: process.env[`SLACK_URL_${i}`]
+          ?[{type: 'slack', url: process.env[`SLACK_URL_${i}`]}]
+          :[]
+      })
+    }
+  }
+})()
